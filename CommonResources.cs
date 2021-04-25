255a7aa7c2dd96f21e19ca4c8016d2d4e184ad4f
Fisayo Oluwayemi (Dev Fishi) <fisayo@wellahealth.com>
2021-04-25 19:04:26 +0100

    added UI and a few logics

---
+++ CommonResources.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WellaWhatsender
+{
+    public class CommonResources
+    {
+        //Message Box
+        public static MessageBoxResult MessageBoxMethod(string messageBoxText, string messageBoxCaption, MessageBoxButton messageBoxButton, MessageBoxImage messageBoxImage)
+        {
+            string messageBoxTextData = messageBoxText;
+            string messageBoxCaptionData = messageBoxCaption;
+            MessageBoxButton messageBoxButtonData = messageBoxButton;
+            MessageBoxImage messageBoxImageData = messageBoxImage;
+            return MessageBox.Show(messageBoxTextData, messageBoxCaptionData, messageBoxButtonData, messageBoxImageData);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WellaWhatsender
 {
@@ -23,6 +10,32 @@ namespace WellaWhatsender
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void PhoneNumberAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(phoneNumber.Text) && !phoneNumbersListBox.Items.Contains(phoneNumber.Text))
+                phoneNumbersListBox.Items.Add(phoneNumber.Text);
+            else
+                CommonResources.MessageBoxMethod("Phone number has already been added", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(messageToSend.Text))
+            {
+                CommonResources.MessageBoxMethod("Please enter a message in the message field provided", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (phoneNumbersListBox.Items.IsEmpty)
+            {
+                CommonResources.MessageBoxMethod("Phone Number List cannot be empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            //WIP
+            foreach (var phoneNumber in phoneNumbersListBox.Items)
+            {
+                //work with the phone numbers here...
+            }
         }
     }
 }
